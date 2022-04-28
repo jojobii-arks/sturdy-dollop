35159e11fb1582098e323de91c5c527625151723
Joseph Bravo <josephangelobravo@gmail.com>
2022-04-28 13:24:48 -0700

    Initial commit

---
+++ svelte.config.js
@@ -0,0 +1,39 @@
+import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-auto';
+import adapterStatic from '@sveltejs/adapter-static';
+
+//? Change when building site.
+//!=================================
+const buildingForGitHubPages = true;
+
+const repositoryName = 'svelte-tailwind';
+//!=================================
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter()
+	},
+
+	preprocess: [
+		preprocess({
+			postcss: true
+		})
+	]
+};
+
+
+if (buildingForGitHubPages) {
+  config.kit.adapter = adapterStatic({
+    pages: 'docs',
+    assets: 'docs',
+  });
+  config.kit.paths = {
+    base: '/' + repositoryName;
+  };
+  config.kit.prerender = {
+    default: true
+  }
+}
+
+export default config;
